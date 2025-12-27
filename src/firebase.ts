c1cc7580a5022594e639ca5f5c490a0feaaa9fa8
CIPHER <birukgetachew253@gmail.com>
2025-12-27 20:39:35 +0300

    Update firebase.ts

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,15 +1,21 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAXMiMckivDSi6bol0EtBafEI3McSNmdkA",
+  apiKey: "AIzaSyAXMiMCkivDSI6bol0EtBafEI3McGNmdkA",
   authDomain: "cipher-studio.firebaseapp.com",
   projectId: "cipher-studio",
   storageBucket: "cipher-studio.firebasestorage.app",
   messagingSenderId: "103353734824",
-  appId: "1:103353734824:web:ae89961928ed5453d5949d"
+  appId: "1:103353734824:web:ae89961928ed5453d5949d",
+  measurementId: "G-DLNXXKH641"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-export const googleProvider = new GoogleAuthProvider();
+const analytics = getAnalytics(app);
